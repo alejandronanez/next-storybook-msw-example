ab213328747f06b27976c50b40757e1367bc6cb9
Alejandro Nanez <git@alejandro.dev>
2022-04-28 17:00:59 -0500

    feat: configure msw/node

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,6 @@
 import '@testing-library/jest-dom/extend-expect'
+import { server } from './mocks/server.js'
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
