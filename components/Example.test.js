3ff1c7119b90325d4e20f50fb3137a3a0e1125dc
Alejandro Nanez <git@alejandro.dev>
2022-04-28 18:13:50 -0500

    feat: plugin @storybook/react-testing
    using stories instead of components. keep the loop tight.

--- components/Example.test.js
+++ components/Example.test.js
@@ -1,6 +1,9 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import { rest, server } from "../mocks/server";
-import { Example } from "./Example";
+import * as stories from "./Example.stories";
+import { composeStories } from "@storybook/testing-react";
+
+const { Success, Error } = composeStories(stories);
 
 describe("Example component", () => {
   it("should work when /api/hello works", async () => {
@@ -10,21 +13,21 @@ describe("Example component", () => {
       })
     );
 
-    render(<Example />);
+    render(<Success />);
 
     await waitFor(() => {
       expect(screen.queryByText(/john doe/i)).toBeInTheDocument();
     });
   });
 
-  it("should work when /api/hello works", async () => {
+  it("should work when /api/hello fails", async () => {
     server.use(
       rest.get("/api/hello", (req, res, ctx) => {
         return res(ctx.status(404));
       })
     );
 
-    render(<Example />);
+    render(<Error />);
 
     await waitFor(() => {
       expect(screen.queryByText(/something bad happened/i)).toBeInTheDocument();
--- jest.setup.js
+++ jest.setup.js
@@ -2,6 +2,14 @@ import "@testing-library/jest-dom/extend-expect";
 import { server } from "./mocks/server.js";
 import "whatwg-fetch";
 
+/**
+ * @storybook/testing-react configuration
+ */
+import { setGlobalConfig } from "@storybook/testing-react";
+import * as globalStorybookConfig from "./.storybook/preview"; // path of your preview.js file
+
+setGlobalConfig(globalStorybookConfig);
+
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
