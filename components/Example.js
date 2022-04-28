2dbfa1ae7a7fe1b039de822bc3e156644290061c
Alejandro Nanez <git@alejandro.dev>
2022-04-28 17:12:21 -0500

    feat: Example component

---
+++ components/Example.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+export function Example() {
+  const [name, setName] = useState("");
+  async function fetchData() {
+    try {
+      const result = await fetch("/hello").then((response) => response.json);
+      setName(result.name);
+    } catch (e) {
+      setName("Something bad happen, try again");
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return <div>Result is: {name}</div>;
+}
