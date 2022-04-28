74f9fcd9cd9b66a05e73d6a1af568395cbbae8de
Alejandro Nanez <git@alejandro.dev>
2022-04-28 17:14:23 -0500

    fix: fix await in Example component

--- components/Example.js
+++ components/Example.js
@@ -4,7 +4,9 @@ export function Example() {
   const [name, setName] = useState("");
   async function fetchData() {
     try {
-      const result = await fetch("/hello").then((response) => response.json);
+      const result = await fetch("/api/hello").then((response) =>
+        response.json()
+      );
       setName(result.name);
     } catch (e) {
       setName("Something bad happen, try again");
